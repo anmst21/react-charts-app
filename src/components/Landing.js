fcc575f3b4c59df1d8777d77cb69c98b1e9fc417
Anthony <anmstudios21c@gmail.com>
2023-02-06 23:50:39 -0500

    no message

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -35,13 +35,13 @@ export default function Landing() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
               <a
-                href="/"
+                href="/charts"
                 className="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
                 Get started
               </a>
               <a
-                href="/"
+                href="/charts"
                 className="text-base font-semibold leading-7 text-white"
               >
                 Learn more <span aria-hidden="true">→</span>
